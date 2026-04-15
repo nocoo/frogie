96ac0e0fecca209cbb3b8689bf58743abf950cf0
nocoo <nicco@duck.com>
2026-04-16 07:32:15 +0800

    Initial commit

--- packages/server/src/db/migrations/003_workspace_color.sql
+++
@@ -1,4 +0,0 @@
--- Migration 003: Add color column to workspaces
--- Allows users to assign a color to each workspace for visual identification
-
-ALTER TABLE workspaces ADD COLUMN color TEXT DEFAULT NULL;
